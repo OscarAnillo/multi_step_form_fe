3a235e8578a7f80e5d300926107ca8ae685e9349
oscar anillo <oscarani14@gmail.com>
2023-01-23 14:10:10 -0500

    add desktop view

--- src/components/form-steps.js
+++ src/components/form-steps.js
@@ -26,9 +26,25 @@ export const FormSteps = ({
     <div className="form-step-one">
       <div className="bg-form-one">
         <button className={`${formStep === 0 ? "one" : "btn"}`}>1</button>
+        <div className="desktop-name">
+          <p>Step 1</p>
+          <h2>Your Info</h2>
+        </div>
         <button className={`${formStep === 1 ? "two" : "btn"}`}>2</button>
+        <div className="desktop-plan">
+          <p>Step 2</p>
+          <h2>Select Plan</h2>
+        </div>
         <button className={`${formStep === 2 ? "three" : "btn"}`}>3</button>
+        <div className="desktop-addon">
+          <p>Step 3</p>
+          <h2>Add-ons</h2>
+        </div>
         <button className={`${formStep === 3 ? "four" : "btn"}`}>4</button>
+        <div className="desktop-summary">
+          <p>Step 4</p>
+          <h2>Summary</h2>
+        </div>
       </div>
       <div className="form-step-div">
         {formStep === 0 && (
--- src/components/thanks-component.js
+++ src/components/thanks-component.js
@@ -2,8 +2,8 @@ export const ThanksComponent = () => {
   return (
     <div className="thanks-div">
       <img src="assets/images/icon-thank-you.svg" alt="" />
-      <h1 className="step-title">Thank you!</h1>
-      <p className="step-text">
+      <h1 className="step-title thanks">Thank you!</h1>
+      <p className="step-text thanks-p">
         Thanks for confirming your subscription! We hope you have fun using our
         platform. If you ever need support, please feel free to email us at
         support@loremgaming.com
